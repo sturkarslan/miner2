27cd4904af80718182c275bafdf33894609333d7
Wei-ju Wu <weiju.wu@gmail.com>
2019-12-09 10:55:36 -0800

    updated coexpression_test

--- test/coexpression_test.py
+++ test/coexpression_test.py
@@ -3,13 +3,16 @@ import sys
 import unittest
 
 import pandas as pd
+import numpy as np
 from miner2 import preprocess
 from miner2 import coexpression
 
 class CoexpressionTest(unittest.TestCase):
 
     def test_cluster(self):
+        ref_exp = pd.read_csv('testdata/expected_pp_exp_data-002.csv', index_col=0, header=0)
         exp, conv_table = preprocess.main('testdata/exp_data-002.csv', 'testdata/conv_table-002.tsv')
+        self.assertTrue(np.isclose(ref_exp, exp).all())  # just to make sure
         expected_clusters = []
         with open('testdata/expected_clusters-002.csv', 'r') as infile:
             for line in infile:
