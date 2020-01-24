ef070b95d5ef21d6e944e964d85bb80bed9e08cd
Wei-ju Wu <weiju.wu@gmail.com>
2020-01-24 13:15:36 -0800

    subtypes generates needed files
      - transcriptional_states.json
      - transcirpitonal_programs.json

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ INSTALL_REQUIRES = ['numpy', 'scipy', 'pandas', 'sklearn', 'lifelines',
                     'pydot', 'graphviz']
 setuptools.setup(
     name="isb_miner2",
-    version="0.1.4",
+    version="0.1.5",
     author="Adrian Lopez Garcia de Lomana",
     author_email="alomana@systemsbiology.org",
     description="A newer version of MINER.",
